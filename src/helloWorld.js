ffc2baacf21faa381fbd4566842a9cb71134b7f2
Mateus Ortiz <mateus.prado@ig.com.br>
2015-05-08 20:21:32 -0300

    Update 2015-05-08T23:21:27.848Z

--- src/helloWorld.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-class Hello extends HTMLElement {
-	createdCallback() {
-		this.textContent = "Hello World, Today's date " + new Date().toJSON().slice(0, 10);
-	}
-}
-
-document.registerElement('hello-world', Hello);
